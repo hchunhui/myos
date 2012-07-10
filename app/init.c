4dca026b2110706c6b9a5181621cd1d4cf0bb4d7
He Chunhui <hchunhui@mail.ustc.edu.cn>
2012-07-10 20:30:44 +0800

    [app] fix init.

--- app/init.c
+++ app/init.c
@@ -7,21 +7,31 @@ char *tty_argv[] = {
 };
 
 extern char **environ;
+int video_fd;
+
+void t_print(char *str)
+{
+	write(video_fd, str, strlen(str));
+}
+
 int main(int argc, char **argv)
 {
 	int ret, status, pid;
+
+	video_fd = open("/dev/video/0", 0);
 	while(1)
 	{
-		printf("spawning tty driver...\n");
+		t_print("spawning tty driver...\n");
 		if((ret = vfork()) == 0)
+		{
+			close(video_fd);
 			exit(execve("/tty.bin", tty_argv, environ));
+		}
 		else if(ret < 0)
 			break;
 		pid = waitpid(-1,&status,0);
-		printf("shell(pid %d) exit with code %d\n",
-		       pid,
-		       status);
+		sprintf(buf, "driver(pid=%d) exit with code %d\n", pid, status);
+		t_print(buf);
 	}
-	printf("can not spawn tty driver.\n");
 	return 1;
 }
