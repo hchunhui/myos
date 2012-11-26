2c785c5aa6fe16b7173ec1617b3a147845d0aaab
He Chunhui <hchunhui@mail.ustc.edu.cn>
2012-11-26 22:44:40 +0800

    [lib] 修复panic

--- lib/klibc.c
+++ lib/klibc.c
@@ -3,16 +3,14 @@
 #include <lib/string.h>
 #include <drv/kdev.h>
 
-void panic(char* text)
-{
-	asm volatile ("cli");	//机器停转
-	//drv_tty_switch(0);
-	printk("\nkernel panic: %s\n", text);
-	for(;;)
-		asm volatile ("hlt");
-}
-
 void print_early(char *obj)
 {
 	dev_simp_write(DEV_MAJOR_VIDEO, 0, video_data, 0, obj, strlen(obj));
+}
+
+void panic(char *text)
+{
+	print = print_early;
+	printk("\nkernel panic: %s\n", text);
+	for(;;);
 }
