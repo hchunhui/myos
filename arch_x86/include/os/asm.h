e166f5cf361c61d92ade3e94f260e26b84af0d0d
He Chunhui <hchunhui@mail.ustc.edu.cn>
2015-10-12 07:45:14 +0000

    [x86] memory barrier for {disable,enable}_irq()

--- arch_x86/include/os/asm.h
+++ arch_x86/include/os/asm.h
@@ -1,8 +1,8 @@
 #ifndef _ASM_H_
 #define _ASM_H_
 #define asmlinkage __attribute__((regparm(0)))
-#define disable_irq() asm("cli")
-#define enable_irq() asm("sti")
+#define disable_irq() asm("cli":::"memory")
+#define enable_irq() asm("sti":::"memory")
 
 #endif
 
