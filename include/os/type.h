8fb0725003e22bd2ab54a8e9f49a97b7772f410b
Chunhui He <hchunhui@mail.ustc.edu.cn>
2017-12-25 14:19:32 +0000

    [os, type] s64, u64

--- include/os/type.h
+++ include/os/type.h
@@ -3,9 +3,11 @@
 typedef unsigned char __u8;
 typedef unsigned short __u16;
 typedef unsigned int __u32;
+typedef unsigned long long __u64;
 typedef signed char __s8;
 typedef signed short __s16;
 typedef signed int __s32;
+typedef signed long long __s64;
 
 #undef __size_t
 #undef __ssize_t
@@ -40,9 +42,11 @@ typedef unsigned int __nlink_t;
 typedef __u8 u8;
 typedef __u16 u16;
 typedef __u32 u32;
+typedef __u64 u64;
 typedef __s8 s8;
 typedef __s16 s16;
 typedef __s32 s32;
+typedef __s64 s64;
 
 typedef __size_t size_t;
 typedef __ssize_t ssize_t;
